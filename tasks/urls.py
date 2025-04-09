ba949f99c79ee74c8def0f870bce42e7a1f6f203
TiaWidi <tia.septianawidi@gmail.com>
2025-04-09 08:27:11 +0700

    Fix ModuleNotFoundError: No module named 'tasks.url'

--- tasks/urls.py
+++ tasks/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('task/create/', views.task_create, name='task_create'),
     path('task/<int:pk>/', views.task_detail, name='task_detail'),
     path('task/<int:pk>/update/', views.task_update, name='task_update'),
-    path('task/<int:pk>/delete/', views.task_delete, name='task_delete')
+    path('task/<int:pk>/delete/', views.task_delete, name='task_delete'),
 ]
